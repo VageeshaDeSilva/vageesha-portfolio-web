eb8bed50caa35b91932c7cf9bfc3e8a62de29816
VageeshaDeSilva <suvadesilva@gmail.com>
2023-12-09 16:23:45 +0530

    testPush

---
+++ js/script.js
@@ -0,0 +1,35 @@
+//  contact form submitBtnOnAction
+//  thanks();
+
+//  navBtnsOnActions
+//  const navLinkEls = document.querySelectorAll('.nav-link');
+//  const sectionEls = document.querySelectorAll('.section');
+//  const activePage = window.location.pathname;   
+
+//  let currentSection = 'home';
+//  window.addEventListener('scroll' , () =>{
+//     sectionEls.forEach(sectionEl => {
+//         if(window.scrollY >= sectionEl.offsetTop){
+//             currentSection = sectionEl.id;
+//         }
+//     });
+
+//     // navLinkEls.forEach(navLinkEl => {
+//     //     if(navLinkEl.href.includes(activePage)){
+//     //         document.querySelector('.active').classList.remove('active');
+//     //         navLinkEl.classList.add('active');
+//     //     }
+//     // })
+
+//  });
+
+//  const mainBtn=document.getElementById("mainBtn");
+
+//  var contactsSection = document.getElementById('contactsSection'); 
+
+//  mainBtn.addEventListener("click", () => { 
+//     // document.body.scrollTo('contacts');
+//     // document.documentElement.scrollTo('contacts');
+// });
+
+ 
